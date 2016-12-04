fc178e10e929cdfe79112873e9c28e8e3d41cc7f
Dave Ebbitt <david.ebbitt@gmail.com>
2016-12-04 12:44:52 +0000

    Fourth Commit (p47)

--- src/pages/landing/landing.ts
+++ src/pages/landing/landing.ts
@@ -1,22 +1,28 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+import { HomePage } from '../home/home';
+import { AuthData } from '../../providers/auth-data';
 
-/*
-  Generated class for the Landing page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
-  selector: 'page-landing',
-  templateUrl: 'landing.html'
+selector: 'page-landing',
+templateUrl: 'landing.html',
 })
+
 export class LandingPage {
+  constructor(public navCtrl: NavController, public authData: AuthData,
+  public loadingCtrl: LoadingController) {}
+  goToLogin(){
+  this.navCtrl.push(LoginPage);
+}
 
-  constructor(public navCtrl: NavController) {}
-
-  ionViewDidLoad() {
-    console.log('Hello LandingPage Page');
+goToBillList(){
+  this.authData.anonymousLogin().then( user => {
+    this.navCtrl.push(HomePage);
+  });
+  let loading = this.loadingCtrl.create({
+    dismissOnPageChange: true
+  });
+  loading.present();
   }
-
 }
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,22 +1,78 @@
+import { 
+  NavController, 
+  LoadingController, 
+  AlertController } from 'ionic-angular';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AuthData } from '../../providers/auth-data';
+import { SignupPage } from '../signup/signup';
+import { HomePage } from '../home/home';
+import { ResetPasswordPage } from '../reset-password/reset-password';
+import { EmailValidator } from '../../validators/email';
 
-/*
-  Generated class for the Login page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-login',
-  templateUrl: 'login.html'
+  templateUrl: 'login.html',
 })
 export class LoginPage {
+  public loginForm;
+  emailChanged: boolean = false;
+  passwordChanged: boolean = false;
+  submitAttempt: boolean = false;
+  loading: any;
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public nav: NavController, public authData: AuthData, 
+    public formBuilder: FormBuilder, public alertCtrl: AlertController, 
+    public loadingCtrl: LoadingController) {
 
-  ionViewDidLoad() {
-    console.log('Hello LoginPage Page');
+    this.loginForm = formBuilder.group({
+      email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],
+      password: ['', Validators.compose([Validators.minLength(6), Validators.required])]
+    });
+  }
+
+  elementChanged(input){
+    let field = input.inputControl.name;
+    this[field + "Changed"] = true;
+  }
+
+  loginUser(){
+
+    this.submitAttempt = true;
+
+    if (!this.loginForm.valid){
+      console.log(this.loginForm.value);
+    } else {
+      this.authData.loginUser(this.loginForm.value.email, this.loginForm.value.password).then( authData => {
+        this.nav.setRoot(HomePage);
+      }, error => {
+        this.loading.dismiss().then( () => {
+          let alert = this.alertCtrl.create({
+            message: error.message,
+            buttons: [
+              {
+                text: "Ok",
+                role: 'cancel'
+              }
+            ]
+          });
+          alert.present();          
+        });
+      });
+
+      this.loading = this.loadingCtrl.create({
+        dismissOnPageChange: true,
+      });
+      this.loading.present();
+    }
+  }
+
+  goToSignup(){
+    this.nav.push(SignupPage);
+  }
+
+  goToResetPassword(){
+    this.nav.push(ResetPasswordPage);
   }
 
 }
