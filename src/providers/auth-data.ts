705d07b5df5ec8590d2fe5a8114943c3435a3fc3
Dave Ebbitt <david.ebbitt@gmail.com>
2016-12-04 11:54:33 +0000

    Third commit

--- src/providers/auth-data.ts
+++ src/providers/auth-data.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
+//import firebase from 'firebase'; 
+// advised by Jorge that AngularFire beta6 already has Firebase.
+// Firebase is imported in the "node_modules/angularfire2/interfaces.d.ts/" file
 
 @Injectable()
 export class AuthData {
@@ -12,4 +15,44 @@ export class AuthData {
       }
     });
   }
+
+  getUser() {
+    return this.fireAuth;
+  }
+
+  loginUser(newEmail: string, newPassword: string): any {
+    return this.af.auth.login({
+      email: newEmail,
+      password: newPassword });
+    }
+
+    anonymousLogin(){
+      return this.af.auth.login({
+        provider: AuthProviders.Anonymous,
+        method: AuthMethods.Anonymous,
+      });
+    }
+
+  linkAccount(email: string, password: string): any {
+    const credential = firebase.auth.EmailAuthProvider.credential(email, password);
+    const userProfile = firebase.database().ref('/userProfile');
+    return this.fireAuth.link(credential).then( (user) => {
+      userProfile.child(user.uid).update({
+        email: email,
+      });
+    }, (error) => {
+        console.log("Account linking error", error);
+      });
+    }
+
+    resetPassword(email: string): any {
+      return firebase.auth().sendPasswordResetEmail(email);
+    }
+
+    logoutUser(): any {
+      return firebase.auth().signOut();
+    }
+
+
+
 }
