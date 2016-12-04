321624c63d48712a5723c304d9568b5ca552ff4f
Dave Ebbitt <david.ebbitt@gmail.com>
2016-12-04 13:15:34 +0000

    Fifth Commit (p57)

--- src/pages/sign-up/sign-up.ts
+++ src/pages/sign-up/sign-up.ts
@@ -1,22 +1,74 @@
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AuthData } from '../../providers/auth-data';
+import { EmailValidator } from '../../validators/email';
+import { HomePage } from '../home/home';
 
-/*
-  Generated class for the SignUp page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
-  selector: 'page-sign-up',
-  templateUrl: 'sign-up.html'
+  selector: 'page-signup',
+  templateUrl: 'sign-up.html',
 })
-export class SignUpPage {
+export class SignupPage {
+  public signupForm;
+  emailChanged: boolean = false;
+  passwordChanged: boolean = false;
+  submitAttempt: boolean = false;
+  loading: any;
 
-  constructor(public navCtrl: NavController) {}
 
-  ionViewDidLoad() {
-    console.log('Hello SignUpPage Page');
+  constructor(public nav: NavController, public authData: AuthData, public formBuilder: FormBuilder,
+    public loadingCtrl: LoadingController, public alertCtrl: AlertController) {
+
+    this.signupForm = formBuilder.group({
+      email: ['', Validators.compose([Validators.required, EmailValidator.isValid])],
+      password: ['', Validators.compose([Validators.minLength(6), Validators.required])]
+    })
   }
 
+  /**
+   * Receives an input field and sets the corresponding fieldChanged property to 'true' to help with the styles.
+   */
+  elementChanged(input){
+    let field = input.inputControl.name;
+    this[field + "Changed"] = true;
+  }
+
+  /**
+   * If the form is valid it will call the AuthData service to sign the user up password displaying a loading
+   *  component while the user waits.
+   *
+   * If the form is invalid it will just log the form value, feel free to handle that as you like.
+   */
+  signupUser(){
+    this.submitAttempt = true;
+
+if (!this.signupForm.valid){
+console.log(this.signupForm.value);
+} else {
+this.authData.linkAccount(this.signupForm.value.email,
+this.signupForm.value.password).then(() => {
+this.nav.setRoot(HomePage);
+}, (error) => {
+this.loading.dismiss().then( () => {
+var errorMessage: string = error.message;
+let alert = this.alertCtrl.create({
+message: errorMessage,
+buttons: [
+{
+text: "Ok",
+role: 'cancel'
 }
+]
+});
+alert.present();
+});
+});
+this.loading = this.loadingCtrl.create({
+dismissOnPageChange: true,
+});
+this.loading.present();
+}
+}
+}
+
